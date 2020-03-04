f67c66c56cee22dff2d961e5abdcb4b3ec30af6e
marc310 <marcelomotta@outlook.com.br>
2020-03-04 11:55:28 -0300

    alterações no form e aplicações do submit no js

--- src/js/multistep.js
+++ src/js/multistep.js
@@ -7,88 +7,122 @@ var steps = $("fieldset").length;
 
 setProgressBar(current);
 
-$(".next").click(function(){
+// eventos de click pra as chamadas
+//
+    // next
+    $(".next").click(function(){
 
-current_fs = $(this).parent();
-next_fs = $(this).parent().next();
+        current_fs = $(this).parent();
+        next_fs = $(this).parent().next();
 
-//Add Class Active
-$("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+        //Add Class Active
+        $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
 
-//show the next fieldset
-next_fs.show();
-//hide the current fieldset with style
-current_fs.animate({opacity: 0}, {
-step: function(now) {
-// for making fielset appear animation
-opacity = 1 - now;
+        //show the next fieldset
+        next_fs.show();
+        //hide the current fieldset with style
+        current_fs.animate({opacity: 0}, {
+        step: function(now) {
+        // for making fielset appear animation
+        opacity = 1 - now;
 
-current_fs.css({
-'display': 'none',
-'position': 'relative'
-});
-next_fs.css({'opacity': opacity});
-scrollToTop(800);
-},
-duration: 500
-});
-setProgressBar(++current);
-});
+        current_fs.css({
+        'display': 'none',
+        'position': 'relative'
+        });
+        next_fs.css({'opacity': opacity});
+        scrollToTop(800);
+        },
+        duration: 500
+        });
+        setProgressBar(++current);
+    });
 
-$(".previous").click(function(){
+    $(".previous").click(function(){
 
-current_fs = $(this).parent();
-previous_fs = $(this).parent().prev();
+        current_fs = $(this).parent();
+        previous_fs = $(this).parent().prev();
 
-//Remove class active
-$("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
+        //Remove class active
+        $("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
 
-//show the previous fieldset
-previous_fs.show();
+        //show the previous fieldset
+        previous_fs.show();
 
-//hide the current fieldset with style
-current_fs.animate({opacity: 0}, {
-step: function(now) {
-// for making fielset appear animation
-opacity = 1 - now;
+        //hide the current fieldset with style
+        current_fs.animate({opacity: 0}, {
+        step: function(now) {
+        // for making fielset appear animation
+        opacity = 1 - now;
 
-current_fs.css({
-'display': 'none',
-'position': 'relative'
-});
-previous_fs.css({'opacity': opacity});
-scrollToTop(600);
-},
-duration: 500
-});
-setProgressBar(--current);
-});
+        current_fs.css({
+        'display': 'none',
+        'position': 'relative'
+        });
+        previous_fs.css({'opacity': opacity});
+        scrollToTop(600);
+        },
+        duration: 500
+        });
+        setProgressBar(--current);
+    });
 
-function setProgressBar(curStep){
-var percent = parseFloat(100 / steps) * curStep;
-percent = percent.toFixed();
-$(".progress-bar")
-.css("width",percent+"%")
-}
-
+    
 $(".submit").click(function(){
-return false;
-})
+    //TODO.. implement submit
+    //
+        // aciona alterações no fieldset pra chamar a proxima tela
+        current_fs = $(this).parent();
+        next_fs = $(this).parent().next();
+
+        //Add Class Active
+        $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+
+        //show the next fieldset
+        next_fs.show();
+        //hide the current fieldset with style
+        current_fs.animate({opacity: 0}, {
+        step: function(now) {
+        // for making fielset appear animation
+        opacity = 1 - now;
+
+        current_fs.css({
+        'display': 'none',
+        'position': 'relative'
+        });
+        next_fs.css({'opacity': opacity});
+        scrollToTop(800);
+        },
+        duration: 500
+        });
+        setProgressBar(++current);
+
+    return false;
+
+});
+
+// progress bar
+function setProgressBar(curStep){
+    var percent = parseFloat(100 / steps) * curStep;
+    percent = percent.toFixed();
+    $(".progress-bar")
+    .css("width",percent+"%")
+}
 
 
 $(window).scroll(function () {
     var top =  $(".goto-top");
         if ( $('body').height() <= (    $(window).height() + $(window).scrollTop() + 200 )) {
-  top.animate({"margin-left": "0px"},1500);
-        } 
-        // else {
-        //     top.animate({"margin-left": "-100%"},1500);
-        // }
-    });
+    top.animate({"margin-left": "0px"},1500);
+    } 
+    // else {
+    //     top.animate({"margin-left": "-100%"},1500);
+    // }
+});
 
-    $(".goto-top").on('click', function () {
-        $("html, body").animate({scrollTop: 0}, 400);
-    });
+$(".goto-top").on('click', function () {
+    $("html, body").animate({scrollTop: 0}, 400);
+});
 
 function scrollToTop(scrollDuration) {
     var scrollStep = -window.scrollY / (scrollDuration / 15),
@@ -101,5 +135,5 @@ function scrollToTop(scrollDuration) {
 }
 
 
-});
+}); // docready
 
